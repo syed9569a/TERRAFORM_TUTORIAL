ec3ce420dd534f0874792696d549c72d23b00ce9
syed9569a <syed9569a@gmail.com>
2020-01-29 08:45:38 +0000

    Terraform init doesnt work

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  version                 = "~> 2.0"
+  version                 = "3.0"
   region                  = "eu-west-1"
   shared_credentials_file = "~/.aws/credentials"
 }
@@ -8,5 +8,4 @@ resource "aws_instance" "EC2" {
   ami           = var.ami-id
   instance_type = var.instance-type
   key_name      = var.pem-key
-
 }
